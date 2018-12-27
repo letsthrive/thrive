2ecf4aa5cef380950526256bab1a1283ccb9f78f
adeelibr <adeelimranr@gmail.com>
2018-12-27 22:52:39 +0500

    initial base setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  parser: 'babylon',
+  tabWidth: 2,
+  semi: true,
+};
