7b6fa328da620d26388b1fad8040f27af840ec8d
adeelibr <adeelimranr@gmail.com>
2018-12-29 01:44:06 +0500

    added app info in manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Thrive`,
+    description: `An open initiative to help find grads/undergrads or people stuck in their dead end jobs. Mentees giving direction to people.`,
+    author: `@adeelibr`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,12 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `lets-thrive`,
+        short_name: `thrive`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `lets-thrive`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
