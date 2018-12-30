a5fc53b8895e02ebd220008416fcb39f543263d7
adeelibr <adeelimranr@gmail.com>
2018-12-30 19:27:33 +0500

    made my name a hyperlink in the footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types'
 // material-ui
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   wrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     marginTop: theme.spacing.unit * 5,
     marginBottom: theme.spacing.unit * 2,
   },
@@ -19,8 +23,16 @@ const Footer = ({ classes }) => (
     <Typography variant="body1" align="center">
       © 2018, Developed With 
       <span role="img" aria-label="love" className={classes.heart}> ♥️ </span> 
-      By Adeel Imran.
+      By 
     </Typography>
+    <Button 
+      component="a" 
+      href="https://twitter.com/adeelibr"
+      target="_blank"
+      color="inherit"
+    >
+      Adeel Imran
+    </Button>
   </footer>
 );
 
