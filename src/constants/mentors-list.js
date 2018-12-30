d759e1fc656a8fa710b5fa1d2b938391bfad1ac5
adeelibr <adeelimranr@gmail.com>
2018-12-31 00:36:53 +0500

    updated mentors-list for farooq

--- src/constants/mentors-list.js
+++ src/constants/mentors-list.js
@@ -82,7 +82,6 @@ export default [
     `,
     types: [
       TYPE.BACKEND_DEVELOPER,
-      TYPE.FRONTEND_DEVELOPER,
     ],
     bio: [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
